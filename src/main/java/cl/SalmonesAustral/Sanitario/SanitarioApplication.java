c7e02692451fd9f40f40b1f511fb001de3ebd5aa
Crainsver <cristobal21rc@gmail.com>
2026-05-29 10:35:34 -0400

    MicroservicioSanitarioCommit

---
+++ src/main/java/cl/SalmonesAustral/Sanitario/SanitarioApplication.java
@@ -0,0 +1,13 @@
+package cl.SalmonesAustral.Sanitario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanitarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanitarioApplication.class, args);
+	}
+
+}
